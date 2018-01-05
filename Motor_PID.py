460498cc83d68939da210d1aaa7060a36b25acd7
ay2718 <ayeiser@mit.edu>
2018-01-04 22:20:54 -0500

    updated comments

--- Motor_PID.py
+++ Motor_PID.py
@@ -2,129 +2,138 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-KP = 40.0;
-KI = 200.0;
-KD = 4.0;
+yn = "Y"
+plt.ion();
+fig = plt.figure();
+ 
+while yn != "n" and yn != "N":
 
-try: KP = float(input("KP = (default is "+str(KP)+") "));
-except: pass
-try: KI = float(input("KP = (default is "+str(KI)+") "));
-except: pass
-try: KD = float(input("KP = (default is "+str(KD)+") "));
-except: pass
+    KP = 40.0;
+    KI = 200.0;
+    KD = 4.0;
 
-time = 5.0
-voltage = 10.0
-motor_kv = 30.0
+    try: KP = float(input("KP = (default is "+str(KP)+") "));
+    except: pass
+    try: KI = float(input("KI = (default is "+str(KI)+") "));
+    except: pass
+    try: KD = float(input("KD = (default is "+str(KD)+") "));
+    except: pass
 
-motor_kv *= (math.pi/30.0)
-motor_r = 0.235
-arm_length = 0.25
-arm_sym_mass = 2.0
-arm_assym_mass = 0.0
-try: arm_assym_mass = float(input("Enter a value for assymetrical arm mass in kg (default is 0) "));
-except: pass
-arm_mass = abs(arm_assym_mass) + arm_sym_mass
-motor_moment = arm_length**2*arm_mass
+    time = 5.0
+    voltage = 10.0
+    motor_kv = 30.0
+
+    motor_kv *= (math.pi/30.0)
+    motor_r = 0.235
+    arm_length = 0.25
+    arm_sym_mass = 2.0
+    arm_assym_mass = 0.0
+    try: arm_assym_mass = float(input("Enter a value for assymetrical arm mass in kg (default is 0) "));
+    except: pass
+    arm_mass = abs(arm_assym_mass) + arm_sym_mass
+    motor_moment = arm_length**2*arm_mass
 
 # step size of physics
-h = 0.001
+    h = 0.001
 
-steps = int(time//h);
-motor_speed_log = np.empty(steps);
-motor_voltage_log = np.empty(steps);
-motor_position_log = np.empty(steps);
-setpoint_log = np.empty(steps);
-resist_log = np.empty(steps);
+    steps = int(time//h);
+    motor_speed_log = np.empty(steps);
+    motor_voltage_log = np.empty(steps);
+    motor_position_log = np.empty(steps);
+    setpoint_log = np.empty(steps);
+    resist_log = np.empty(steps);
 
 # step size of control circuitry
-arduino_h = 0.01
-arduino_steps = int(arduino_h // h);
+    arduino_h = 0.01
+    arduino_steps = int(arduino_h // h);
 
 # delay between changing motor speed and receiving data
-delay = 0.03
-delay_steps = int(delay // h);
+    delay = 0.03
+    delay_steps = int(delay // h);
 
-motor_speed = 0.0
-motor_voltage = 0.0
-motor_position = 0.0
+    motor_speed = 0.0
+    motor_voltage = 0.0
+    motor_position = 0.0
 
 # gives motor acceleration as a function of a bunch of parameters
-def motor_acc(resistance):
-    global motor_voltage;
-    global motor_speed;
-    return ((motor_voltage - motor_speed/motor_kv)/(motor_kv*motor_r) - resistance)/motor_moment
+    def motor_acc(resistance):
+        global motor_voltage;
+        global motor_speed;
+        return ((motor_voltage - motor_speed/motor_kv)/(motor_kv*motor_r) - resistance)/motor_moment
 
-def physics_step(i):
-    global motor_speed
-    global motor_position
-    motor_speed_log[i] = motor_speed;
-    motor_position_log[i] = motor_position;
-    motor_voltage_log[i] = motor_voltage;
-    setpoint_log[i] = setpoint(i);
-    resist_log[i] = 10*arm_length*arm_assym_mass*math.sin(motor_position);
+    def physics_step(i):
+        global motor_speed
+        global motor_position
+        motor_speed_log[i] = motor_speed;
+        motor_position_log[i] = motor_position;
+        motor_voltage_log[i] = motor_voltage;
+        setpoint_log[i] = setpoint(i);
+        resist_log[i] = 10*arm_length*arm_assym_mass*math.sin(motor_position);
 
-    kmotor = np.empty(4);
-    kmotorpos = np.empty(4);
+        kmotor = np.empty(4);
+        kmotorpos = np.empty(4);
 
-    kmotor[0] = motor_acc(resist_log[i]);
-    kmotorpos[0] = motor_speed;
-    motor_speed = motor_speed_log[i] + kmotor[0]*h/2;
-    motor_position = motor_position_log[i] + kmotorpos[0]*h/2;
-    
-    kmotor[1] = motor_acc(resist_log[i]);
-    kmotorpos[1] = motor_speed;
-    motor_speed = motor_speed_log[i] + kmotor[1]*h/2;
-    motor_position = motor_position_log[i] + kmotorpos[1]*h/2;
-    
-    kmotor[2] = motor_acc(resist_log[i]);
-    kmotorpos[2] = motor_speed;
-    motor_speed = motor_speed_log[i] + kmotor[2]*h;
-    motor_position = motor_position_log[i] + kmotorpos[2]*h/2;
+        kmotor[0] = motor_acc(resist_log[i]);
+        kmotorpos[0] = motor_speed;
+        motor_speed = motor_speed_log[i] + kmotor[0]*h/2;
+        motor_position = motor_position_log[i] + kmotorpos[0]*h/2;
+        
+        kmotor[1] = motor_acc(resist_log[i]);
+        kmotorpos[1] = motor_speed;
+        motor_speed = motor_speed_log[i] + kmotor[1]*h/2;
+        motor_position = motor_position_log[i] + kmotorpos[1]*h/2;
+        
+        kmotor[2] = motor_acc(resist_log[i]);
+        kmotorpos[2] = motor_speed;
+        motor_speed = motor_speed_log[i] + kmotor[2]*h;
+        motor_position = motor_position_log[i] + kmotorpos[2]*h/2;
 
-    kmotor[3] = motor_acc(resist_log[i]);
-    kmotorpos[3] = motor_speed;
-    motor_speed = motor_speed_log[i] + (kmotor[0] + kmotor[3] + 2*(kmotor[1] + kmotor[2]))*h/6;
-    motor_position = motor_position_log[i] + (kmotorpos[0] + kmotorpos[3] + 2*(kmotorpos[1] + kmotorpos[2]))*h/6;
+        kmotor[3] = motor_acc(resist_log[i]);
+        kmotorpos[3] = motor_speed;
+        motor_speed = motor_speed_log[i] + (kmotor[0] + kmotor[3] + 2*(kmotor[1] + kmotor[2]))*h/6;
+        motor_position = motor_position_log[i] + (kmotorpos[0] + kmotorpos[3] + 2*(kmotorpos[1] + kmotorpos[2]))*h/6;
 
-shelf = 1.0 
-def setpoint(curr_step):
-    setp = 0.0;
-    if curr_step*h > shelf:
-        setp = math.pi/2;
-    else:
+    shelf = 1.0 
+    def setpoint(curr_step):
         setp = 0.0;
-    return setp;
+        if curr_step*h > shelf:
+            setp = math.pi/2;
+        else:
+            setp = 0.0;
+        return setp;
 
-integral_error = 0.0;
+    integral_error = 0.0;
 
-def error(i):
-    global integral_error;
-    err = setpoint(i) - motor_position;
-    k = i - arduino_steps
-    if k < 0: k = 0;
-    derivative_error = (motor_position_log[k] - motor_position)/arduino_h;
-    if abs(KD*derivative_error) < 10.0 and abs(KP*err) < 10.0: integral_error += arduino_h*err
-    correction = KP*err + KI*integral_error + KD*derivative_error;
-    return correction;
+    def error(i):
+        global integral_error;
+        err = setpoint(i) - motor_position;
+        k = i - arduino_steps
+        if k < 0: k = 0;
+        derivative_error = (motor_position_log[k] - motor_position)/arduino_h;
+        if abs(KD*derivative_error) < 10.0 and abs(KP*err) < 10.0: integral_error += arduino_h*err
+        correction = KP*err + KI*integral_error + KD*derivative_error;
+        return correction;
+        
+    def trim(value):
+        if value > 10.0: value = 10.0;
+        if value < -10.0: value = -10.0;
+        return value;
+
+    for i in range(0, steps):
+        physics_step(i);
+        if i % arduino_steps == 0:
+            j = i - delay_steps;
+            if j < 0: j = 0;
+            motor_voltage = error(i);
+            motor_voltage = trim(motor_voltage);
     
-def trim(value):
-    if value > 10.0: value = 10.0;
-    if value < -10.0: value = -10.0;
-    return value;
+    yn = input("Clear previous plots (Y/n) ");
+    if yn != "n" and yn != "N":
+        plt.clf();
+        plt.plot(h*np.arange(0, steps), setpoint_log, color = "k", linewidth=2);
 
-for i in range(0, steps):
-    physics_step(i);
-    if i % arduino_steps == 0:
-        j = i - delay_steps;
-        if j < 0: j = 0;
-        motor_voltage = error(i);
-        motor_voltage = trim(motor_voltage);
+    fig.suptitle("KP = "+str(KP)+" V/(rad),    KI = "+str(KI)+" V/(rad*s),    KD = "+str(KD)+" V/(rad/s),    Arm Mass = "+str(arm_assym_mass)+" kg", fontsize = 20)
+    plt.plot(h*np.arange(0, steps), motor_position_log);
+    plt.show();
 
-_, plots = plt.subplots(2, sharex = True);
-plots[0].plot(h*np.arange(0, steps), motor_position_log);
-plots[0].plot(h*np.arange(0, steps), setpoint_log);
-plots[1].plot(h*np.arange(0, steps), motor_voltage_log);
-plots[0].set_title("Motor Position vs Time")
-plots[1].set_title("Motor Voltage vs Time")
-plt.show();
+    yn = input("Run again? (Y/n) ");
--- PID.py
+++ PID.py
@@ -1,15 +1,32 @@
+#
+#
+#
+#
+# Author: Aaron Yeiser
+# PID.py simulates a quadcopter that is being PID tuned
+# The user can also add noise or systematic error
+#
+#
+# 
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# PID coefficients for position
 stab_p = 0.0;
 stab_d = 0.0;
+
+# PID coefficients for angular velocity
 KP = 0.05;
 KI = 0.2;
 KD = 0.002;
+
+# bias is systematic error, noise is noise level in gyroscope
 bias = 0.0;
 noise = False;
 
+# Allow user to change PID coefficients from defaults
 try: KP = float(input("KP = (default is "+str(KP)+") "));
 except: pass
 try: KI = float(input("KP = (default is "+str(KI)+") "));
@@ -17,24 +34,32 @@ except: pass
 try: KD = float(input("KP = (default is "+str(KD)+") "));
 except: pass
 
+# User can add systematic error (default is no error)
 yn = input("Would you like to add a systematic error (y/N)? ")
 if yn == 'y' or yn == 'Y':
     bias = 0.02;
 
+# User can add random noise (default is no noise)
 yn = input("Would you like to add random noise (y/N)? ")
 if yn == 'y' or yn == 'N':
     noise = True;
 
+# User can force quadcopter to add an addition position stabilization loop (default is no)
 try: stab_p = float(input("Position stabilization P coefficient (default is 0) "));
 except: pass
 
+# Motors operate at 50% thrust by default
 base_thrust = 0.5;
 
+# Simulation runs for time, motors operate at voltage, and motor rotates at kv rpm per volt under no load
 time = 5.0
 voltage = 10.0
 motor_kv = 1090.0
 
+# convert to rad/s/volt
 motor_kv *= (math.pi/30.0)
+
+# motor resistance, moments of inertia
 motor_r = 0.235
 quad_moment = 0.004
 prop_moment = 5e-5
@@ -46,6 +71,7 @@ quad_position = 0.0
 # step size of physics
 h = 0.001
 
+# Logging
 steps = int(time//h);
 quad_speed_log = np.empty(steps);
 quad_position_log = np.empty(steps);
@@ -91,19 +117,25 @@ def prop_acc():
             pac[i] = ((prop_voltage[i] - prop_speed[i]/motor_kv)/(motor_kv*motor_r) - prop_res_const*prop_speed[i]**2)/prop_moment
     return pac;
 
+# returns quadcopter angular acceleration
 def quad_acc():
     return 2*(prop_speed[1]**2 - prop_speed[0]**2)*prop_thr_const*arm_length/prop_moment
 
+# coupled Runge-Kutta solve for quadcopter position and speed and propeller speed
 def physics_step(i):
     global quad_speed
     global quad_position
     global prop_speed
+
+    # Log current prop speed values
+    # Log values get used as temp variables here
     quad_speed_log[i] = quad_speed;
     quad_position_log[i] = quad_position;
     prop_speed_log[i, :] = prop_speed;
     prop_voltage_log[i, :] = prop_voltage;
     setpoint_log[i] = setpoint(i);
 
+    # Runge Kutta magic happens
     kprop = np.empty((4, 2));
     kquadspeed = np.empty(4);
     kquadpos = np.empty(4);
@@ -142,6 +174,7 @@ def physics_step(i):
     quad_speed = quad_speed_log[i] + (kquadspeed[0] + kquadspeed[3] + 2*(kquadspeed[1] + kquadspeed[2]))*h/6;
     quad_position = quad_position_log[i] + (kquadpos[0] + kquadpos[3] + 2*(kquadpos[1] + kquadpos[2]))*h/6;
 
+# setpoint function to force quadcopter to rotate at a specific time
 shelf = 2.0
 def setpoint(curr_step):
     if curr_step*h > shelf:
@@ -151,32 +184,45 @@ def setpoint(curr_step):
 
 integral_error = 0.0;
 
+# gives quadcopter KP*proportional error + KI*integral error + KD*derivative error
 def error(i):
     global integral_error;
     qspeed = quad_speed;
+
+    # Allows for noise to be simulated
     if noise: qspeed += np.random.normal(0.0, 0.001);
     err = 0.0;
+
+    # Different methods of stabilizing the quadcopter
     if stab_p != 0:
         err = qspeed - stab_p*(setpoint(i) - quad_position) + stab_d*quad_speed;
     else:
         err = qspeed - setpoint(i);
+
+    # Introduce delay
     k = i - arduino_steps
     if k < 0: k = 0;
+
+    # compute error
     integral_error += arduino_h*err
     derivative_error = (qspeed - quad_speed_log[k])/arduino_h;
     correction = KP*err + KI*integral_error + KD*derivative_error;
     return correction;
     
+# trims a value to between 0 and 1
 def trim(value):
     if value > 1.0: value = 1.0;
     if value < 0.0: value = 0.0;
     return value;
 
+# simulates quadcopter
 for i in range(0, steps):
     physics_step(i);
     if i % arduino_steps == 0:
         j = i - delay_steps;
         if j < 0: j = 0;
+
+        # add systematic error, if present
         correction = error(i) + bias;
         thrust[0] = base_thrust + correction;
         thrust[1] = base_thrust - correction;
